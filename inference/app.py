2fb2f14364ce6b7f163ccc848f405c06d74cee15
ModiCnvrg <modi.tamam@cnvrg.io>
2024-06-10 11:40:05 +0300

    Added gpt2 serving and required dependencies

--- inference/app.py
+++ inference/app.py
@@ -1,5 +1,34 @@
-import sys
-print(sys.path)
-import numpy as np
-print(np.__version__)
-print(np.zeros(5))
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from flask import Flask, request, jsonify
+import os
+
+app = Flask(__name__)
+current_directory = os.path.dirname(os.path.realpath(__file__))
+
+# Load the model and tokenizer
+model_name = 'gpt2'
+model_dir = os.path.join(current_directory, 'model')
+model = GPT2LMHeadModel.from_pretrained(model_dir)
+tokenizer = GPT2Tokenizer.from_pretrained(model_dir)
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    inputs = data.get('inputs')
+    parameters = data.get('parameters', {})
+
+    # Tokenize input
+    input_ids = tokenizer.encode(inputs, return_tensors='pt')
+
+    # Generate text
+    output = model.generate(input_ids, **parameters)
+    output_text = tokenizer.decode(output[0], skip_special_tokens=True)
+
+    return jsonify({'generated_text': output_text})
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
